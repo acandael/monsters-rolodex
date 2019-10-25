ddf3b11cb8047d99fd2276d514c764680aaf11be
acandael <info@anthonycandaele.com>
2019-10-25 13:14:34 +0200

    Updates

---
+++ precache-manifest.8ddeafe056bb4e7c14e493f23f5ec4e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16eb870b0e21d892b9946a83e283662f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a0f0d6cc6d9ea27b2cee",
+    "url": "/monsters-rolodex/static/css/main.79017815.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "a0f0d6cc6d9ea27b2cee",
+    "url": "/monsters-rolodex/static/js/main.227b6a10.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
